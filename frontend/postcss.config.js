5bcd27ddd8999152e101d7187817fdb96d302fc2
Shahin <cps2@atlantic724.ca>
2025-05-01 10:49:37 -0400

    Fix blog interface errors to allow production build

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+module.exports = {
+    plugins: {
+      '@tailwindcss/postcss': {},
+      autoprefixer: {},
+      // Add any other PostCSS plugins you were using here
+    },
+  };
