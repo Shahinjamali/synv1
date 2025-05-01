73154aeda8f651f6ff22fa5e708af634898a1071
Shahin <cps2@atlantic724.ca>
2025-04-30 22:34:53 -0400

    Fix: remove empty interface in blogs/page.tsx for production build

--- frontend/src/app/dashboard/owner/[role]/categories/[slug]/edit/page.tsx
+++ frontend/src/app/dashboard/owner/[role]/categories/[slug]/edit/page.tsx
@@ -1,1 +1,6 @@
 // Edit categories by slug\n\nexport default function EditCategoriesPage() {\n  return <div>Edit categories page</div>;\n}
+'use client';
+
+export default function CategoryEditPage() {
+  return <h1>Product Edit Page</h1>;
+}
